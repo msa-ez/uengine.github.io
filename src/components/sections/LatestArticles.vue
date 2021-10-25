3fe8167ad692b1a4e28ebfdbeeda226b1022d11d
kibum0405 <kibum0405@uengine.org>
2021-10-25 10:25:01 +0900

    git init

---
+++ src/components/sections/LatestArticles.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+    <div class="container px-5 py-12 mx-auto">
+      <div class="text-center pb-12 ">
+        <h2 class="text-3xl">
+          제품 서비스 영역
+        </h2>
+        <p
+          class="text-1xl">
+          유엔진솔루션즈는 2003년 이래 업무 프로세스 관리 (Business Process Management),
+          기업용 SNS, PaaS 를 포함한 클라우드 플랫폼,
+          도메인 기반 개발 방법을 지향하는 개발 프레임워크 등을 개발하고 오픈소스 프로젝트로 제공하는 기술 집약적 기업입니다.
+        </p>
+      </div>
+      <div class="flex flex-wrap -m-4">
+        <RecordCard
+          v-for="edge in $static.records.edges"
+          :key="edge.node.id"
+          :record="edge.node" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<static-query>
+  query {
+    records: allArticle(limit:6, sortBy:"createdAt") {
+      edges {
+        node {
+          title
+          path
+          excerpt
+          createdAt(format:"Do MMMM YYYY")
+          timeToRead
+        }
+      }
+    }
+  }
+</static-query>
+
+<script>
+import RecordCard from '~/components/RecordCard'
+
+export default {
+  components: {
+    RecordCard
+  }
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,71 @@
+<template>
+  <Layout>
+    <PageHeader style="height:600px;">
+      <template v-slot:image>
+        <g-image style="-webkit-filter: blur(5px); -moz-filter: blur(5px); -o-filter: blur(5px); -ms-filter: blur(5px); filter: blur(5px);" src="~/assets/images/marvin-meyer-SYTO3xs06fU-unsplash.jpg" />
+      </template>
+      <template v-slot:content>
+        <p class="text-4xl md:text-6xl">
+          유엔진 오픈소스
+        </p>
+        <p class="text-lg md:text-4xl">
+          <br/>Cloud Platform  |  Enterprise SNS  |  SaaS Billing  |  UI/UX Platform
+        </p>
+        <p class="text-lg md:text-2xl">
+          <br/>the Universal Engines for Digital Economy Era
+        </p>
+        <br/>
+        <div style="width:50%; margin-left:25%;">
+          <!-- <p class="text-lg md:text-2xl text-left">
+            사용자 인터뷰
+          </p> -->
+          <iframe style="width:100%; height:300px;"
+            src="https://www.youtube.com/embed/ioOdnl_aIKE"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen
+          >
+          </iframe>
+        </div>
+      </template>
+    </PageHeader>
+    <div class="container text-center mx-auto">
+      <SectionLatestArticles />
+    </div>
+
+    <SectionContribute />
+
+    <SectionThinkDifferent />
+    
+    <div class="container text-center mx-auto">
+      <SectionLatestNews />
+    </div>
+
+    
+
+    <SectionOpenSource />
+  </Layout>
+</template>
+
+<script>
+import PageHeader from '~/components/PageHeader'
+import SectionLatestArticles from '~/components/sections/LatestArticles'
+import SectionLatestNews from '~/components/sections/LatestNews'
+import SectionContribute from '~/components/sections/Contribute'
+import SectionThinkDifferent from '~/components/sections/ThinkDifferent'
+import SectionOpenSource from '~/components/sections/OpenSource'
+
+export default {
+  metaInfo: {
+    title: 'Learn something new'
+  },
+  components: {
+    PageHeader,
+    SectionLatestArticles,
+    SectionLatestNews,
+    SectionContribute,
+    SectionThinkDifferent,
+    SectionOpenSource
+  }
+};
+</script>
